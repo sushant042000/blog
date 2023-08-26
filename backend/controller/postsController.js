fc38aaaa64f3508baa80885d7abb020a5bd82e52
Sushant Bailkar <sushantbailkar2504@gmail.com>
2023-08-26 22:06:11 +0530

    error Handling

--- backend/controller/postsController.js
+++ backend/controller/postsController.js
@@ -1,126 +1,122 @@
 const Post = require("../models/postModel");
 const cloudinary = require("cloudinary");
+const ErrorHandler = require("../utils/custumError");
+const asyncErrorHandler = require("../utils/asyncErrorHandler");
 
-exports.createPost = async (req, res) => {
-  try {
-    const { title, description } = req.body;
-    const id = req.user._id;
 
-    const myCloud = await cloudinary.v2.uploader.upload(req.body.image, {
-      folder: "blog_images",
-      width: 150,
-      crop: "scale",
-    });
 
-    console.log("my", myCloud);
+exports.createPost = asyncErrorHandler(async (req, res, next) => {
+  const requiredFields = ["title", "description", "image"];
+  const missingFields = requiredFields.filter((field) => !req.body[field]);
 
-    const post = await Post.create({
-      title,
-      content: description,
-      author: id,
-      image: {
-        public_id: myCloud.public_id,
-        url: myCloud.secure_url,
-      },
-    });
-    const data = await post.save();
-    return res.status(200).json({
-      success: "true",
-      data,
-    });
-  } catch (error) {
-    if (error.name === "ValidationError") {
-      const validationErrors = {};
-      for (const field in error.errors) {
-        validationErrors[field] = error.errors[field].message;
-      }
-      return res.status(400).json({
-        validationErrors,
-      });
-    }
-    return res.status(500).json({
-      error,
-    });
+  if (missingFields.length > 0) {
+    const errorMessage = `${missingFields.join(",")} is required`;
+
+    next(new ErrorHandler(errorMessage, 400));
   }
-};
+  const { title, description, category, image } = req.body;
 
-exports.updatePost = async (req, res) => {
-  try {
-    const { id } = req.params;
-    console.log("body",req.body)
+  const id = req.user._id;
 
-    const myCloud = await cloudinary.v2.uploader.upload(req.body.image, {
-      folder: "blog_images",
-      width: 150,
-      crop: "scale",
-    });
+  const myCloud = await cloudinary.v2.uploader.upload(image, {
+    folder: "blog_images",
+    width: 150,
+    crop: "scale",
+  });
+
+  const post = await Post.create({
+    title,
+    content: description,
+    author: id,
+    category,
+    image: {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    },
+  });
+  const data = await post.save();
+
+  return res.status(200).json({
+    success: "true",
+    data,
+  });
+});
 
 
-    const dataToUpdate = {
-      title: req.body.title,
-      content: req.body.description,
-      image: {
-        public_id: myCloud.public_id,
-        url: myCloud.secure_url,
-      },
-    };
-    
-    const updatedData = await Post.findByIdAndUpdate({ _id: id }, dataToUpdate, {
-      new: true,
-      runValidators: true,
-      useFindAndModify: false,
-    });
 
-    // console.log("updated",updatedData)
 
-    return res.status(200).json({
-      success: "true",
-      updatedData,
-    });
-  } catch (error) {
-    return res.status(200).json({
-      error,
-    });
+exports.updatePost = asyncErrorHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const requiredFields = ["title", "description", "image"];
+  const missingFields = requiredFields.filter((field) => !req.body[field]);
+
+  if (missingFields.length > 0) {
+    const errorMessage = `${missingFields.join(",")} is required`;
+
+    next(new ErrorHandler(errorMessage, 400));
   }
-};
 
-exports.allPosts = async (req, res) => {
-  try {
-    const posts = await Post.find();
+  const { image, description, title } = req.body;
 
-    return res.status(200).json({
-      success: "true",
-      posts,
-    });
-  } catch (error) {
-    return res.status(200).json({
-      error,
-    });
+  const myCloud = await cloudinary.v2.uploader.upload(image, {
+    folder: "blog_images",
+    width: 150,
+    crop: "scale",
+  });
+
+  if (!myCloud) {
+    next(new ErrorHandler("Internal server Error", 500));
   }
-};
 
-exports.deletePost = async (req, res) => {
-  try {
-    console.log("htr")
-    const { id } = req.params;
-    console.log("id",id)
-    const isExist=await Post.findOne({_id:id});
+  const dataToUpdate = {
+    title: title,
+    content: description,
+    image: {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    },
+  };
 
-    if(!isExist){
-      return res.status(404).json({
-        message:"post not found"
-      });
-    }
+  const updatedData = await Post.findByIdAndUpdate({ _id: id }, dataToUpdate, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
 
-    const data = await Post.findByIdAndDelete({_id:id});
+  return res.status(200).json({
+    success: "true",
+    updatedData,
+  });
+});
 
-    return res.status(200).json({
-      success: "true",
-      message:"post deleted successfully"
-    });
-  } catch (error) {
-    return res.status(200).json({
-      error,
-    });
+
+
+
+exports.allPosts = asyncErrorHandler(async (req, res) => {
+  const posts = await Post.find();
+
+  return res.status(200).json({
+    success: "true",
+    posts,
+  });
+});
+
+
+
+
+exports.deletePost = asyncErrorHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const isExist = await Post.findOne({ _id: id });
+
+  if (!isExist) {
+    next(new ErrorHandler("Post not found", 404));
   }
-};
+
+  const data = await Post.findByIdAndDelete({ _id: id });
+
+  return res.status(200).json({
+    success: "true",
+    message: "post deleted successfully",
+  });
+});
